df92f66531c3d6d61f37e263f8e9736f71d97a08
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-02-22 07:52:01 -0700

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+let a = 2000;
+let b = 100;
+let divide = function() {
+    return a / b;
+}
+
+let x = 2;
+const square = (x) => x * x;
+
+let parameterA = 3;
+let parameterB = 4;
+let add= (parameterA, parameterB) => parameterA + parameterB;
